a41b098e5b83957e2e9daa47b54c78973272f51e
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-07-16 18:20:40 +0300

    Auto copy fonts

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 // Import React and ReactDOM
 import React from 'react';
-import ReactDOM from 'react-dom/cjs/react-dom.production.min';
+import ReactDOM from 'react-dom';
 
-// Import F7
+// Import Framework7
 import Framework7 from 'framework7/framework7.esm.bundle';
 
-// Import F7 React Plugin
+// Import Framework7-React plugin
 import Framework7React from 'framework7-react';
 
-// Import main App Component
+// Import main App component
 import App from './components/App.jsx';
 
-// Framework7 styles and icons
+// Framework7 styles
 import 'framework7/css/framework7.min.css';
-import 'framework7-icons/css/framework7-icons.css';
-import 'material-design-icons/iconfont/material-icons.css';
 
-// Init F7 React Plugin
+// Icons
+import './css/icons.css';
+
+// Custom app styles
+import './css/app.css';
+
+// Init Framework7-React plugin
 Framework7.use(Framework7React);
 
 // Mount React App
