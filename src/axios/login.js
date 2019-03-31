8cb6dfc0b9c3ebd4065a800783aceddd78eeb8f6
Daniil <daniil@rivc-pulkovo.ru>
2019-03-31 10:01:29 +0300

    axios

--- src/axios/login.js
+++ src/axios/login.js
@@ -1,44 +1,21 @@
-import { set, get } from 'idb-keyval';
-import { axios } from './init'
+const axios = require('axios');
 
 
-export class authorisation {
+export class auth {
 
-    login = async (...args) => {
-        const url = '/login';
-        const payload = {
-            email: args[0],
-            password: args[1],
+    login = (...args) => {
+        // const {login, password} = args;
+        console.log(args);
+        axios.post('https://marshal.bh-app.ru/api/login', {
+            email: 'test@test.ru',
+            password: '12345',
             application: 'marshal'
-        };
-        let res = 'test';
-        await get('AUTH_TOKEN').then( async value => {
-            if (value === undefined) {
-                try {
-                    let response = await axios.post(url, payload);
-                    let AUTH_TOKEN = response.data.access_token;
-                    axios.defaults.headers.common['Authorization'] = 'Bearer ' + AUTH_TOKEN;
-                    set('AUTH_TOKEN', AUTH_TOKEN).then();
-                    res = await this.user_details();
-
-                } catch (error) {
-                    res = error.response;
-                }
-            }else{
-                res = await this.user_details();
-                console.log(res);
-            }
-        });
-        return res;
-    };
-
-    user_details = async () => {
-        const url = '/details';
-        const payload = {};
-        try {
-            return await axios.get(url, payload);
-        } catch (error) {
-            return error.response;
-        }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 }
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import {connect} from "react-redux";
-import { set } from 'idb-keyval';
 import {
     Page,
     List,
@@ -8,77 +6,20 @@ import {
     Button,
     BlockFooter,
     Link,
-    LoginScreenTitle,
-    Navbar
+    LoginScreenTitle
 } from 'framework7-react';
 
-import { authorisation } from '../../axios/login'
-import { handleLogin } from '../../actions/UserActions'
+import {auth} from '../../axios/login'
 
 class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputLogin: '',
-            inputPassword: '',
-        };
+
+    authentication() {
+        const test = new auth();
+        test.login('login', 'password');
     }
-
-    authError = this.$f7.notification.create({
-        icon: '<i class="icon marshal-icon"> </i>',
-        title: 'Маршал Сервис',
-        subtitle: 'Ошибка авторизации',
-        text: 'Проверьте имя пользователя и пароль.',
-        closeButton: true,
-    });
-
-    authSuccess = (name) => this.$f7.notification.create({
-        icon: '<i class="icon marshal-icon"> </i>',
-        title: 'Маршал Сервис',
-        subtitle: 'Добро пожаловать ' + name + '!',
-        closeTimeout: 3000,
-    });
-
-    handleOnChange = prop => event => {
-      this.setState({[prop]: event.target.value});
-    };
-
-    async authentication() {
-        const self = this;
-        self.$f7.dialog.preloader('Пожалуйста подождите...');
-        set('AUTH_TOKEN', undefined).then();
-        set('user', undefined).then();
-        const auth = new authorisation();
-        const { inputLogin, inputPassword } = this.state;
-        const response = await auth.login(inputLogin, inputPassword);
-        if (response.status === 401) {
-            this.authError.open();
-        } else if (response.status === 200) {
-            this.authSuccess(response.data.success.name).open();
-            this.$f7.views.main.router.navigate('/');
-        }
-        self.$f7.dialog.close();
-        this.props.handleLogin(response.data.success);
-    }
-
-    register_user() {
-        this.$f7.views.main.router.navigate('/edit_user/');
-    }
-
     render() {
-
-        const { inputLogin, inputPassword} = this.state;
-
         return (
             <Page loginScreen>
-                <Navbar
-                    color="white"
-                    textColor="white"
-                    bgColor="main"
-                    title=''
-                    backLink="Back"
-                >
-                </Navbar>
                 <LoginScreenTitle>Войти</LoginScreenTitle>
                 <List form>
                     <ListInput
@@ -86,16 +27,12 @@ class LoginPage extends React.Component {
                         name="username"
                         placeholder="Email"
                         type="email"
-                        value={inputLogin}
-                        onChange={this.handleOnChange('inputLogin')}
                     />
                     <ListInput
                         label="Password"
                         name="password"
                         placeholder="Password"
                         type="password"
-                        value={inputPassword}
-                        onChange={this.handleOnChange('inputPassword')}
                     />
 
                     <BlockFooter>
@@ -105,24 +42,13 @@ class LoginPage extends React.Component {
 
                 <List>
                     <BlockFooter>
-                        <p><Link onClick={()=>this.register_user()}>Зарегистрироваться</Link></p>
-                        {/*<p>Для восстановления пароля нажмите <Link>ссылку</Link></p>*/}
+                        <p><Link>Зарегистрироваться</Link></p>
+                        <p>Для восстановления пароля нажмите <Link>ссылку</Link></p>
                     </BlockFooter>
                 </List>
             </Page>
         )
     }
 }
-const mapStateToProps = store => {
-    return {
-        user: store.user,
-    }
-};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        handleLogin: user => dispatch(handleLogin(user)),
-    }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginPage)
+export default (LoginPage)
