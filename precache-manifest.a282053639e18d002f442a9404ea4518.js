9a8de29728480237b5afa62e2bf30517f6958380
Daniil <daniil@rivc-pulkovo.ru>
2019-04-22 00:06:59 +0300

    Updates

---
+++ precache-manifest.a282053639e18d002f442a9404ea4518.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c473c8e9fe7aeae5021",
+    "url": "./static/css/main.2bd0eb8f.chunk.css"
+  },
+  {
+    "revision": "8c473c8e9fe7aeae5021",
+    "url": "./static/js/main.1cde9484.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "f346a5b1df1b003b0909",
+    "url": "./static/css/2.dd06cacc.chunk.css"
+  },
+  {
+    "revision": "f346a5b1df1b003b0909",
+    "url": "./static/js/2.4405f4a9.chunk.js"
+  },
+  {
+    "revision": "efdffb4a57443b99c56893e74c4cc622",
+    "url": "./static/media/icon.efdffb4a.svg"
+  },
+  {
+    "revision": "0a51e7bba83f6ce6ef65b35a9ba4e0f2",
+    "url": "./static/media/Framework7Icons-Regular.0a51e7bb.woff2"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "0cefb5cc6ff6df9509ed193cff79de7c",
+    "url": "./static/media/Framework7Icons-Regular.0cefb5cc.woff"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "d5e7e689e790dd5f60eaa438da65581f",
+    "url": "./static/media/Framework7Icons-Regular.d5e7e689.eot"
+  },
+  {
+    "revision": "61bbf26d99172b62fb8eb5b03585db72",
+    "url": "./static/media/Framework7Icons-Regular.61bbf26d.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "./static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "dd9a7fd9d4322c7e6c820c557f54923e",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/marshal/precache-manifest.cfcee4970bb1902c136250b6dc637d46.js"
+  "./precache-manifest.a282053639e18d002f442a9404ea4518.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/marshal/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
